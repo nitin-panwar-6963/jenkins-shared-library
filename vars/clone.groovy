00926c3e23380fd0caf0e6e5e366c079bc33344c
Nitin panwar <nitinpanwar6963@gmail.com>
2025-11-24 08:34:05 +0530

    Update clone.groovy

--- vars/clone.groovy
+++ vars/clone.groovy
@@ -1,6 +1,6 @@
 def call(String url , String branch){
   echo "cloning the code ......"
   sh "sleep 5s"
-  git branch: "${branch}" , "url: ${url}"
+  git branch: '${branch}' , url: '${url}'
   echo "cloning is succesfull ......"
 }
