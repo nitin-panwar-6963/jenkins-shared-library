c57e776eec2855a68c4c641b870b9208463ce071
Nitin panwar <nitinpanwar6963@gmail.com>
2025-11-24 08:54:25 +0530

    Update build.groovy

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,5 +1,5 @@
 def call(String image){
   echo "build your custom image...."
   sh "sleep 5s"
-  sh "docker build -t $(image) . "
+  sh "docker build -t ${image} . "
   echo "your image is buil succesfully...."
