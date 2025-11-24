b8d406407e7372b6b00178123f61f47c79262b79
Nitin panwar <nitinpanwar6963@gmail.com>
2025-11-24 09:12:03 +0530

    Update deploy.groovy

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,6 +1,6 @@
 def call(){
   echo "your app deployment is starting...."
   sh "sleep 5s"
-  sh "docker compose up -d "
+  sh "docker compose up -d --build"
   echo "your app is deploy succesfull"
 }
